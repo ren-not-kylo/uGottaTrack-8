a2c574ea0be5fc4711b063483ab4f450d0292beb
Chelsea <chelsea.ren.b@gmail.com>
2024-03-03 03:49:02 -0500

    move ball

---
+++ canvas.js
@@ -0,0 +1,9 @@
+/*let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+window.addEventListener('load',() =>{
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(3, 3, 3, 0, Math.PI * 2);
+    ctx.fill();
+});*/
